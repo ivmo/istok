e7db183ecd78e0bf1c5348af456e641cb48d0915
Ivan Klimukhin <ivankmn@yandex.ru>
2019-05-30 00:58:53 +0300

    create gh-pages

--- src/js/lib.js
+++
@@ -1,9 +0,0 @@
-//=  libs/jquery-3.3.1.js
-//=  libs/slick.js
-//=  libs/wow.min.js
-//=  libs/cookie.js
-//=  libs/jquery.fancybox.js
-//=  libs/jquery.maskedinput.js
-//=  libs/mcsbar.min.js
-//=  libs/imageloaded.js
-//=  libs/masonry.pkgd.min.js
